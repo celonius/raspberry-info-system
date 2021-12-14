d8275c74fa5cd917df1f6e490a61be4a72443a28
celonius <63497135+celonius@users.noreply.github.com>
2021-12-14 09:28:25 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo apt update -y && sudo apt upgrade -y && sudo apt install gcc python3-dev && pip3 install psutil && sudo apt autoremove -y && cp /home/$USER/raspberry-info-system/RPi_info_system.1.0.desktop /home/$USER/Desktop/RPi_info_system.1.0.desktop && chmod +x /home/$USER/Desktop/RPi_info_system.1.0.py  
+sudo apt update -y && sudo apt upgrade -y && sudo apt install gcc python3-dev && pip3 install psutil && sudo apt autoremove -y && cp /home/$USER/raspberry-info-system/RPi_info_system.1.0.desktop /home/$USER/Desktop/RPi_info_system.1.0.desktop && chmod +x /home/$USER/Desktop/RPi_info_system.1.0.desktop  
